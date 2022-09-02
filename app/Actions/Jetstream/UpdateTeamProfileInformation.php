512f9cd561eeb8263dbdac1d0f075a491b911320
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-09-02 11:49:44 +0100

    Changes

--- app/Actions/Jetstream/UpdateTeamProfileInformation.php
+++ app/Actions/Jetstream/UpdateTeamProfileInformation.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Jetstream;
 
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
@@ -15,11 +16,11 @@ class UpdateTeamProfileInformation
      * Validate and update Team profile information
      *
      * @param Request $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      * @throws AuthorizationException
      * @throws ValidationException
      */
-    public function update(Request $request): \Illuminate\Http\RedirectResponse
+    public function update(Request $request): RedirectResponse
     {
         $user = $request->user();
         $team = Jetstream::newTeamModel()->findOrFail($user->current_team_id);
--- app/Http/Controllers/TeamDashController.php
+++ app/Http/Controllers/TeamDashController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Inertia\Response;
 use Laravel\Jetstream\Jetstream;
 use Laravel\Jetstream\RedirectsActions;
 
@@ -33,6 +34,74 @@ class TeamDashController extends Controller
             'defaultPermissions' => Jetstream::$defaultPermissions,
         ]);
 
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function showSettings(Request $request): Response
+    {
+        $team = Jetstream::newTeamModel()->findOrFail($request->user()->current_team_id);
+        Gate::authorize('view', $team);
+
+        return Jetstream::inertia()->render($request, 'Team/Settings/Show', [
+            'user' => $request->user(),
+            'team' => $team,
+            'permissions' => [
+                'canAddTeamMembers' => Gate::check('addTeamMember', $team),
+                'canDeleteTeam' => Gate::check('delete', $team),
+                'canRemoveTeamMembers' => Gate::check('removeTeamMember', $team),
+                'canUpdateTeam' => Gate::check('update', $team),
+            ],
+            'availableRoles' => array_values(Jetstream::$roles),
+            'availablePermissions' => Jetstream::$permissions,
+            'defaultPermissions' => Jetstream::$defaultPermissions,
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function showMembers(Request $request): Response
+    {
+        $team = Jetstream::newTeamModel()->findOrFail($request->user()->current_team_id);
+        Gate::authorize('view', $team);
+
+        return Jetstream::inertia()->render($request, 'Team/Members/Show', [
+            'user' => $request->user(),
+            'team' => $team,
+            'permissions' => [
+                'canAddTeamMembers' => Gate::check('addTeamMember', $team),
+                'canDeleteTeam' => Gate::check('delete', $team),
+                'canRemoveTeamMembers' => Gate::check('removeTeamMember', $team),
+                'canUpdateTeam' => Gate::check('update', $team),
+            ],
+            'availableRoles' => array_values(Jetstream::$roles),
+            'availablePermissions' => Jetstream::$permissions,
+            'defaultPermissions' => Jetstream::$defaultPermissions,
+        ]);
+    }
+
+    public function showStats(Request $request)
+    {
+        $team = Jetstream::newTeamModel()->findOrFail($request->user()->current_team_id);
+        Gate::authorize('view', $team);
+
+        return Jetstream::inertia()->render($request, 'Team/Members/Show', [
+            'user' => $request->user(),
+            'team' => $team,
+            'permissions' => [
+                'canAddTeamMembers' => Gate::check('addTeamMember', $team),
+                'canDeleteTeam' => Gate::check('delete', $team),
+                'canRemoveTeamMembers' => Gate::check('removeTeamMember', $team),
+                'canUpdateTeam' => Gate::check('update', $team),
+            ],
+            'availableRoles' => array_values(Jetstream::$roles),
+            'availablePermissions' => Jetstream::$permissions,
+            'defaultPermissions' => Jetstream::$defaultPermissions,
+        ]);
     }
 
 
