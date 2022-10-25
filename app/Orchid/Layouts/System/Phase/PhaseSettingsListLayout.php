549d0db9d9a41621da9da4d01d96c9bf04a29d07
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-10-25 09:41:24 +0100

    Add orchid/crud + working on delete actions

--- app/Orchid/Layouts/System/Phase/PhaseSettingsListLayout.php
+++ app/Orchid/Layouts/System/Phase/PhaseSettingsListLayout.php
@@ -3,12 +3,15 @@
 namespace App\Orchid\Layouts\System\Phase;
 
 use App\Models\PhaseSetting;
+use Orchid\Platform\Models\User;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Fields\Matrix;
 use Orchid\Screen\Layouts\Table;
 use Orchid\Screen\TD;
 use Orchid\Support\Facades\Layout;
+use Orchid\Support\Facades\Toast;
 
 class PhaseSettingsListLayout extends Table
 {
@@ -22,9 +25,9 @@ class PhaseSettingsListLayout extends Table
         return
             [
                 TD::make('name', __('Name'))
-                ->sort()
-                ->cantHide()
-                ->filter(Input::make()),
+                    ->sort()
+                    ->cantHide()
+                    ->filter(Input::make()),
 
                 TD::make('data_type', __('Type'))
                     ->sort()
@@ -38,13 +41,13 @@ class PhaseSettingsListLayout extends Table
 
                 TD::make('values', __('Values'))
                     ->cantHide()
-                    ->render(function (PhaseSetting $target){
+                    ->render(function (PhaseSetting $target) {
 
-                        $arr = (array) json_decode($target->values, true);
+                        $arr = (array)json_decode($target->values, true);
                         $output = '';
-                        foreach ( $arr as $key => $val) {
-                            $output .= '<li> Value: ' . $val['Value'] .'</li>
-                                        <li>Attribute: ' . $val['Attribute'] .'</li>' ;
+                        foreach ($arr as $key => $val) {
+                            $output .= '<li> Value: ' . $val['Value'] . '</li>
+                                        <li>Attribute: ' . $val['Attribute'] . '</li>';
                         }
                         return $output;
 
@@ -55,12 +58,18 @@ class PhaseSettingsListLayout extends Table
                        return Button::make()
                             ->icon('trash')
                             ->confirm(__('Once the account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.'))
-                            ->method('remove', [
-                                'id' => $target->id,
-                            ]);
+                            ->method('remove');
                     }),
 
             ];
+    }
+
+    public function remove(PhaseSetting $phaseSetting)
+    {
+        $phaseSetting->delete();
+
+        Toast::info(__('Phase Setting was removed'));
+
     }
 }
 
