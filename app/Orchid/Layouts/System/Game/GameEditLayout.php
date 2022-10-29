7bf9181a79a4567f6f8e18d6427d6547393c1954
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-10-29 23:25:20 +0100

    Finish Game Edit/Create Forms

--- app/Orchid/Layouts/System/Game/GameEditLayout.php
+++ app/Orchid/Layouts/System/Game/GameEditLayout.php
@@ -2,19 +2,48 @@
 
 namespace App\Orchid\Layouts\System\Game;
 
+use App\Models\Game;
+use Orchid\Screen\Field;
+use Orchid\Screen\Fields\CheckBox;
+use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Layouts\Rows;
-use Orchid\Screen\TD;
 
 class GameEditLayout extends Rows
 {
 
-
+    /**
+     * Views.
+     *
+     * @return Field[]
+     */
     protected function fields(): iterable
-    {        return [
+    {
+        return [
 
-        TD::make('game_name', 'name')
-            ->sort(),
+            Input::make('game.game_title')
+                ->type('text')
+                ->max(255)
+                ->required()
+                ->title(__('Game Title')),
 
-    ];// TODO: Implement fields() method.
+            Input::make('game.available_platforms')
+                ->type('text')
+                ->max(255)
+                ->required()
+                ->title(__('Available Platforms')),
+
+            Input::make('game.game_photo_path')
+                ->type('text')
+                ->max(255)
+                ->required()
+                ->title(__('No function currently')),
+
+            Checkbox::make('game.state_active')
+                ->placeholder('Enable')
+                ->required()
+                ->sendTrueOrFalse()
+                ->title(__('Enables ability to create tournaments for this game')),
+
+        ];
     }
 }
