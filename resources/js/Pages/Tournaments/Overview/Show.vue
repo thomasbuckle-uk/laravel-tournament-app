23bfdacc7a67409ecae8ee61ca36efaa393002ac
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-11-26 16:52:22 +0000

    WIP: Tournament Dashboard List view

--- resources/js/Pages/Tournaments/Overview/Show.vue
+++ resources/js/Pages/Tournaments/Overview/Show.vue
@@ -1,22 +1,24 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Jetstream/Welcome.vue';
+import TournamentList from '../Partials/TournamentList.vue'
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-
+Test
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-xl sm:rounded-lg">
-
+                    <TournamentList></TournamentList>
                 </div>
             </div>
         </div>
+
+
     </AppLayout>
 </template>
---
+++ resources/js/Pages/Tournaments/Partials/TournamentList.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+const items = [
+    { id: 1 },
+    { id: 2}
+    // More items...
+]
+</script>
+
+<template>
+    <ul role="list" class="space-y-3">
+        <li v-for="item in items" :key="item.id" class="overflow-hidden bg-transparent border-gray-700 border-[2px] px-4 py-4 shadow sm:rounded-md sm:px-6">
+            <!-- Your content -->
+        </li>
+    </ul>
+</template>
+
+
+<script>
+</script>
+<style scoped>
+
+</style>
