42d721782b096e6f16f6f5f88c6d4909858516bc
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-11-28 23:49:20 +0000

    Updates

--- resources/js/Pages/Tournaments/Details/Show.vue
+++ resources/js/Pages/Tournaments/Details/Show.vue
@@ -1,13 +1,40 @@
-<template>
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import TournamentList from '../Partials/TournamentList.vue'
 
-</template>
 
-<script>
-export default {
-    name: "Show"
-}
+defineProps({
+    upcomingTournaments: Array,
+    permissions: Object,
+});
 </script>
 
-<style scoped>
+<template>
+    <AppLayout title="Dashboard">
+        <div class="container w-full center mt-10">
 
-</style>
+            <div class="mt-24">
+                <div class="overflow-hidden rounded-md bg-black bg-opacity-75  border border-[1px] border-gray-500">
+                    <div class="px-4 py-5 sm:p-6">
+                        <!-- Content goes here -->
+                    </div>
+                </div>
+                <div class="overflow-hidden rounded-md bg-black bg-opacity-75 mt-10 border border-[1px] border-gray-500">
+                    <div class="px-4 py-5 sm:p-6">
+                        <!-- Content goes here -->
+                    </div>
+                </div>
+                <div class="overflow-hidden rounded-md bg-black bg-opacity-75 mt-10  border border-[1px] border-gray-500">
+                    <div class="px-4 py-5 sm:p-6">
+                        <!-- Content goes here -->
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+    </AppLayout>
+</template>
--- resources/js/Pages/Tournaments/Overview/Show.vue
+++ resources/js/Pages/Tournaments/Overview/Show.vue
@@ -16,21 +16,19 @@ defineProps({
             <div class="mt-24 flex justify-center">
                 <div class="text-lg max-w-prose mx-auto">
                     <h1>
-                        <span class="mt-2 block text-xl text-center leading-8 font-medium tracking-tight text-white sm:text-4xl">Tournaments</span>
+                        <span
+                            class="mt-2 block text-xl text-center leading-8 font-medium tracking-tight text-white sm:text-4xl">Tournaments</span>
                     </h1>
                     <p class="mt-4 text-center text-gray-500 leading-8"></p>
                 </div>
             </div>
 
-                    <TournamentList
-                        :tournaments="upcomingTournaments"
-                    ></TournamentList>
-
+            <TournamentList
+                :tournaments="upcomingTournaments"
+            ></TournamentList>
 
 
         </div>
-
-
 
 
     </AppLayout>
