f8ed11efca72293d17a996e653158e70149dcf2d
Thomas Buckle <thomasbuckle.uk@outlook.com>
2022-11-29 00:10:01 +0000

    WIP tournament details page

--- resources/js/Pages/Tournaments/Details/Show.vue
+++ resources/js/Pages/Tournaments/Details/Show.vue
@@ -1,10 +1,11 @@
 <script setup>
+import {Head, Link} from '@inertiajs/inertia-vue3';
+
 import AppLayout from '@/Layouts/AppLayout.vue';
-import TournamentList from '../Partials/TournamentList.vue'
 
 
 defineProps({
-    upcomingTournaments: Array,
+    tournament: Object,
     permissions: Object,
 });
 </script>
@@ -15,9 +16,20 @@ defineProps({
 
             <div class="mt-24">
                 <div class="overflow-hidden rounded-md bg-black bg-opacity-75  border border-[1px] border-gray-500">
-                    <div class="px-4 py-5 sm:p-6">
-                        <!-- Content goes here -->
-                    </div>
+                   <div class="grid grid-cols-4">
+                       <div class="ml-10 py-20 col-span-2 ">
+                           <img class="rounded-md bg-no-repeat" src="/assets/images/warzone-cover.jpg" alt="">
+                       </div>
+                       <div class="py-5 sm:p-6 grid grid-cols-1 items-center h-50">
+                           <h1 class="text-white"> {{tournament.long_name}}</h1>
+                           <Link
+                               class="text-white bg-[#C10013] rounded-md py-2 px-5">
+                               Join
+                           </Link>
+                       </div>
+
+
+                   </div>
                 </div>
                 <div class="overflow-hidden rounded-md bg-black bg-opacity-75 mt-10 border border-[1px] border-gray-500">
                     <div class="px-4 py-5 sm:p-6">
